7bd7cb5f5c96d1d0e9ac1b48ff6fedb2294d137b
MushfiqueAlam <mushfique.037@gmail.com>
2025-03-10 15:11:53 +0530

    add book related service and controller

---
+++ src/main/java/com/bookStore/service/BookService.java
@@ -0,0 +1,28 @@
+package com.bookStore.service;
+
+import com.bookStore.entity.Book;
+import com.bookStore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    @Autowired
+    BookRepository bookRepository;
+    public void addBook(Book book){
+        bookRepository.save(book);
+    }
+
+    public List<Book> getAllBook(){
+        return bookRepository.findAll();
+    }
+    public Book getBookById(int id){
+        return bookRepository.findById(id).get();
+    }
+
+    public void deleteBook(int id){
+        bookRepository.deleteById(id);
+    }
+}
